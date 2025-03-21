3c6c15c6c2b735c576eaf4d6f44745b682db8c60
Gleb Kushedow <kushedow@gmail.com>
2025-03-21 16:52:44 +0300

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cors Proxy for Google Apps Scripts doGet / doPosts endpoint
+
+Send request to /<app_id> , get it converted to 
+
+```
+https://script.google.com/macros/s/{app_id}/exec
+```
+
+* CORS Allow All
+* Supports redirects
+* JSON ONLY
---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
+import httpx
+import logging
+
+app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+async def create_httpx_client():
+    """Creates an httpx client with desired configurations."""
+    return httpx.AsyncClient(
+        follow_redirects=True,
+        timeout=60.0,
+    )
+
+@app.get("/{serviceid:str}")
+async def get_proxy(serviceid: str, request: Request, client: httpx.AsyncClient = Depends(create_httpx_client)):
+    """Proxies GET requests to the Google Apps Script web app."""
+    target_url = f"https://script.google.com/macros/s/{serviceid}/exec"
+    query_params = request.query_params
+
+    if query_params:
+      target_url += "?" + str(query_params) # string conversion required
+
+    logging.info(f"GET Proxying to: {target_url}")
+
+    try:
+        response = await client.get(target_url)
+        logging.info(response.text)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+        return response.json()
+
+    except httpx.HTTPStatusError as e:
+        logging.error(f"HTTP error: {e}")
+        raise HTTPException(status_code=e.response.status_code, detail=str(e))
+    except httpx.RequestError as e:
+        logging.error(f"Request error: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        logging.exception(f"Unexpected error: {e}")
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
